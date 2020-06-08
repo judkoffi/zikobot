eb703eabc118a3d5165084d0b6c3abe53e7a9245
judi <judkoffi@gmail.com>
2020-06-08 18:54:06 +0200

    update ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,10 @@
 import { Client } from 'discord.js';
+import { Helper } from '../utils/helper';
 
 export async function readyHandler(client: Client) {
-  console.log(`Logged in as ${client.user?.tag}!`);
+  console.log(`Logged in as ${client?.user?.tag}!`);
   await client.user.setPresence({
     status: 'online',
-    activity: { name: '?help'}
+    activity: { name: `${Helper.PREFIX}help` }
   });
 }
